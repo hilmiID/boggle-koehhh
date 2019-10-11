30f5d2f0e5f9365f2ff6efb3da57acc14aabc943
robithritz <robithritz@gmail.com>
2019-10-11 13:08:04 +0700

    add class active on click

--- src/js/event.js
+++ src/js/event.js
@@ -9,45 +9,44 @@ const actions = document.querySelectorAll('.panel-command>button');
 
 console.log(words);
 console.log(actions);
-
 const one$ = fromEvent(words[0], 'click').pipe(
-    map((v) => { return { letter: words[0].textContent, position: { x: 0, y: 0 } } }))
+    map((v) => { return { senderIndex: 0, letter: words[0].textContent, position: { x: 0, y: 0 } } }))
 const two$ = fromEvent(words[1], 'click').pipe(
-    map((v) => { return { letter: words[1].textContent, position: { x: 1, y: 0 } } }))
+    map((v) => { return { senderIndex: 1, letter: words[1].textContent, position: { x: 1, y: 0 } } }))
 const three$ = fromEvent(words[2], 'click').pipe(
-    map((v) => { return { letter: words[2].textContent, position: { x: 2, y: 0 } } }));
+    map((v) => { return { senderIndex: 2, letter: words[2].textContent, position: { x: 2, y: 0 } } }));
 const four$ = fromEvent(words[3], 'click').pipe(
-    map((v) => { return { letter: words[3].textContent, position: { x: 3, y: 0 } } }));
+    map((v) => { return { senderIndex: 3, letter: words[3].textContent, position: { x: 3, y: 0 } } }));
 const five$ = fromEvent(words[4], 'click').pipe(
-    map((v) => { return { letter: words[4].textContent, position: { x: 0, y: 1 } } }));
+    map((v) => { return { senderIndex: 4, letter: words[4].textContent, position: { x: 0, y: 1 } } }));
 const six$ = fromEvent(words[5], 'click').pipe(
-    map((v) => { return { letter: words[5].textContent, position: { x: 1, y: 1 } } }));
+    map((v) => { return { senderIndex: 5, letter: words[5].textContent, position: { x: 1, y: 1 } } }));
 const seven$ = fromEvent(words[6], 'click').pipe(
-    map((v) => { return { letter: words[6].textContent, position: { x: 2, y: 1 } } }));
+    map((v) => { return { senderIndex: 6, letter: words[6].textContent, position: { x: 2, y: 1 } } }));
 const eight$ = fromEvent(words[7], 'click').pipe(
-    map((v) => { return { letter: words[7].textContent, position: { x: 3, y: 1 } } }));
+    map((v) => { return { senderIndex: 7, letter: words[7].textContent, position: { x: 3, y: 1 } } }));
 const nine$ = fromEvent(words[8], 'click').pipe(
-    map((v) => { return { letter: words[8].textContent, position: { x: 0, y: 2 } } }));
+    map((v) => { return { senderIndex: 8, letter: words[8].textContent, position: { x: 0, y: 2 } } }));
 const ten$ = fromEvent(words[9], 'click').pipe(
-    map((v) => { return { letter: words[9].textContent, position: { x: 1, y: 2 } } }));
+    map((v) => { return { senderIndex: 9, letter: words[9].textContent, position: { x: 1, y: 2 } } }));
 const eleven$ = fromEvent(words[10], 'click').pipe(
-    map((v) => { return { letter: words[10].textContent, position: { x: 2, y: 2 } } }));
+    map((v) => { return { senderIndex: 10, letter: words[10].textContent, position: { x: 2, y: 2 } } }));
 const twelve$ = fromEvent(words[11], 'click').pipe(
-    map((v) => { return { letter: words[11].textContent, position: { x: 3, y: 2 } } }));
+    map((v) => { return { senderIndex: 11, letter: words[11].textContent, position: { x: 3, y: 2 } } }));
 const thirteen$ = fromEvent(words[12], 'click').pipe(
-    map((v) => { return { letter: words[12].textContent, position: { x: 0, y: 3 } } }));
+    map((v) => { return { senderIndex: 12, letter: words[12].textContent, position: { x: 0, y: 3 } } }));
 const fourteen$ = fromEvent(words[13], 'click').pipe(
-    map((v) => { return { letter: words[13].textContent, position: { x: 1, y: 3 } } }));
+    map((v) => { return { senderIndex: 13, letter: words[13].textContent, position: { x: 1, y: 3 } } }));
 const fifteen$ = fromEvent(words[14], 'click').pipe(
-    map((v) => { return { letter: words[14].textContent, position: { x: 2, y: 3 } } }));
+    map((v) => { return { senderIndex: 14, letter: words[14].textContent, position: { x: 2, y: 3 } } }));
 const sixteen$ = fromEvent(words[15], 'click').pipe(
-    map((v) => { return { letter: words[15].textContent, position: { x: 3, y: 3 } } }));
+    map((v) => { return { senderIndex: 15, letter: words[15].textContent, position: { x: 3, y: 3 } } }));
 
 const check$ = fromEvent(actions[0], 'click').pipe(map(v => 'check'));
 const clean$ = fromEvent(actions[1], 'click').pipe(map(v => 'clean'));
 const next$ = fromEvent(actions[2], 'click').pipe(map(v => 'next'));
 
-const screen = document.querySelector('.panel-validation>h1');
+const screen = document.querySelector('.panel-validation');
 console.log(screen);
 const listResultPanel = document.querySelector('.panel-list');
 
@@ -66,11 +65,24 @@ let score = 0;
 words$.subscribe((v) => {
     if (lastPos) {
         // check valid tetangga;
+        validasiTetangga({ letter: v.letter, initialPosition: { x: v.position.x, y: v.position.y }, beforePosition: lastPos })
+            .then((res) => {
+                if (res) {
+                    lastPos = v.position;
+                    input += v.letter;
+                    screen.textContent = input;
+                    console.log(input, lastPos);
+                    words[v.senderIndex].classList.add("active");
+                }
+            })
+    } else {
+        lastPos = v.position;
+        input += v.letter;
+        screen.textContent = input;
+        console.log(input, lastPos);
+        words[v.senderIndex].classList.add("active");
     }
-    lastPos = v.position;
-    input += v.letter;
-    screen.textContent = input;
-    console.log(input, lastPos);
+
 })
 
 actions$.subscribe((v) => {
